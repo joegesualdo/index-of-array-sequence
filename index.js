71f8f30cf5ced27332143b3e89ff4991d448eb66
Joe Gesualdo <joegesualdo@gmail.com>
2015-09-29 11:19:32 -0400

    Fix bug that wasn't finding sequences with a single element

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function indexOfArrySequence(searchSequence, source){
   for(var i = 0; i < source.length; i++){
     if(searchSequence[0] == source[i]){
-      for(var x = 1; x < searchSequence.length; x++){
+      for(var x = 0; x < searchSequence.length; x++){
         if(searchSequence[x] == source[i+x]){
           if(x == (searchSequence.length- 1)){
             return i 
