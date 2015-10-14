6bae192d7cde1c35f52c1584775445d429afa320
Nyaundi Brian <ndieksman@gmail.com>
2015-10-14 22:47:27 +0300

    use of strict equality operators for comparison

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function indexOfArrySequence(searchSequence, source){
   for(var i = 0; i < source.length; i++){
-    if(searchSequence[0] == source[i]){
+    if(searchSequence[0] === source[i]){
       for(var x = 0; x < searchSequence.length; x++){
-        if(searchSequence[x] == source[i+x]){
+        if(searchSequence[x] === source[i+x]){
           if(x == (searchSequence.length- 1)){
             return i 
           }else{
